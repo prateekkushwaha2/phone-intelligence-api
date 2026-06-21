f9282365639a49b29b7e7962b9d94db49301332b
Prateek Kushwaha <83070760+prateekkushwaha2@users.noreply.github.com>
2026-06-21 12:30:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+from fastapi import FastAPI
+from routes.phone import router
 
+app = FastAPI(
+  title="phone Intelligence API",
+  version="1.0"
+)  
+
+app.include_router(router)
+
+@app.get("/")
+def home():
+  return {
+    "message": "Phone Intelligence API Running"
+  }
+  
